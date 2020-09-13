87430e4f887bf104191dd3a591a9ec8b981a9d43
Shafiq Islam <71161633+shafiqislam1st@users.noreply.github.com>
2020-09-13 13:37:31 +0600

    sharif task ... add basic algorithm

---
+++ Tech_Sharif/Basic_Algorithm/add_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    cout << sum << "\n";
+
+    return 0;
+}
